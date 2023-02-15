dfaf3c7fd659a701e95a1cdb7c6acf1b3d6593df
mohamed <mohamed.lajmi@esprit.tn>
2023-02-15 11:04:54 +0100

    first commit

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/Model/Todo';
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css'],
+})
+export class TodosComponent implements OnInit {
+  todos: Todo[] = [];
+  inputTodo: string = '';
+  constructor() {}
+  ngOnInit(): void {
+    this.todos = [
+      {
+        content: 'todo1',
+        completed: false,
+      },
+      {
+        content: 'todo2',
+        completed: false,
+      },
+    ];
+  }
+  toggleDone(id: any) {
+    this.todos.map((o, i) => {
+      if (i == id) o.completed = !o.completed;
+      return o;
+    });
+  }
+  delete(id: any) {
+    this.todos = this.todos.filter((o, i) => id == !i);
+  }
+  addTodo() {
+    this.todos.push({
+      content: this.inputTodo,
+      completed: false,
+    });
+
+    this.inputTodo = '';
+  }
+}
